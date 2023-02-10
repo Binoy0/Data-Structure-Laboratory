05856916f758f304078a153574c4e64620a786c2
Binoy0 <108354873+Binoy0@users.noreply.github.com>
2023-02-11 00:50:53 +0600

    Add files via upload

---
+++ 13-06-2022.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+const float pi=3.1416;
+int main()
+{
+    int radius;
+    float area;
+    area= (pi*radius*radius);
+    printf("Area of circle=%2f",area);
+
+    return 0;
+}
